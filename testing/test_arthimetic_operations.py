409d172b772cc95aed04a13e2e96da0664849475
Aklile Yilma <aklileyilma@gmail.com>
2023-03-28 14:15:40 +0300

    write test_divide, test_subtract, test_multiply

--- testing/test_arthimetic_operations.py
+++ testing/test_arthimetic_operations.py
@@ -19,3 +19,50 @@ class TestArithmeticClass:
 
         assert expected_output == actual_output
         
+    def test_subtract(self):
+        # arrange
+        x, y = 1.0, 2.0
+        expected_output = -1.0
+        # act
+        actual_output = ArthimeticClass.subtract(x, y)
+        # assert
+        with pytest.raises(TypeError):
+            ArthimeticClass.add("1", 2)
+        with pytest.raises(TypeError):
+            ArthimeticClass.add(1, "2")
+        with pytest.raises(TypeError):
+            ArthimeticClass.add("1", "2")
+
+        assert expected_output == actual_output
+        
+    def test_multiply(self):
+        # arrange
+        x, y = 1.0, 2.0
+        expected_output = 2.0
+        # act
+        actual_output = ArthimeticClass.multiply(x, y)
+        # assert
+        with pytest.raises(TypeError):
+            ArthimeticClass.add("1", 2)
+        with pytest.raises(TypeError):
+            ArthimeticClass.add(1, "2")
+        with pytest.raises(TypeError):
+            ArthimeticClass.add("1", "2")
+
+        assert expected_output == actual_output
+        
+    def test_divide(self):
+        # arrange
+        x, y = 1.0, 2.0
+        expected_output = 0.5
+        # act
+        actual_output = ArthimeticClass.divide(x, y)
+        # assert
+        with pytest.raises(TypeError):
+            ArthimeticClass.add("1", 2)
+        with pytest.raises(TypeError):
+            ArthimeticClass.add(1, "2")
+        with pytest.raises(TypeError):
+            ArthimeticClass.add("1", "2")
+
+        assert expected_output == actual_output
